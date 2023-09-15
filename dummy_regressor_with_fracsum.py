4e9c95b82606ecf0fae2a208d7e9ee1b7c62f123
1p3p37 <nejdan4ik86@gmail.com>
2023-09-15 22:50:51 +0300

    feat: updated dummy_regressor_with_fracsum in accordance with the specifications

--- dummy_regressor_with_fracsum.py
+++ dummy_regressor_with_fracsum.py
@@ -1,5 +1,9 @@
 import numpy as np
 from sklearn.dummy import DummyRegressor
+from sklearn.utils import check_array, check_consistent_length
+from sklearn.utils.validation import _check_sample_weight
+from sklearn.utils.stats import _weighted_percentile
+
 
 class DummyRegressorWithFracSum(DummyRegressor):
     """
@@ -65,24 +69,67 @@ class DummyRegressorWithFracSum(DummyRegressor):
         self : object
             Fitted estimator.
         """
-        # super()._validate_params()
+        y = check_array(y, ensure_2d=False, input_name="y")
+        if len(y) == 0:
+            raise ValueError("y must not be empty.")
 
-        # Calculate the sum of fractional parts
-        # fractional_sum = np.sum(np.mod(y, 1))
-        fractional_sum = np.sum(y - np.floor(y))
-
-
-        # Store the result as the constant_
-        self.constant_ = np.array([fractional_sum])
-
-        # n_outputs_ количество выходных переменных (число регрессионных целей)
-        """
-        y.ndim возвращает количество измерений (dimensions) в массиве y. 
-        Если y - одномерный массив, то y.ndim вернет 1, а если y - двумерный массив, то вернет 2.
-        """
         if y.ndim == 1:
-            self.n_outputs_ = 1
-        else:
-            self.n_outputs_ = y.shape[1] # количества столбцов 
+            y = np.reshape(y, (-1, 1))
+        self.n_outputs_ = y.shape[1]
 
+        check_consistent_length(X, y, sample_weight)
+
+        if sample_weight is not None:
+            sample_weight = _check_sample_weight(sample_weight, X)
+
+        if self.strategy == "mean":
+            self.constant_ = np.average(y, axis=0, weights=sample_weight)
+
+        if self.strategy == "fracsum":
+            self.constant_ = np.sum(y - np.floor(y))
+
+        elif self.strategy == "median":
+            if sample_weight is None:
+                self.constant_ = np.median(y, axis=0)
+            else:
+                self.constant_ = [
+                    _weighted_percentile(y[:, k], sample_weight, percentile=50.0)
+                    for k in range(self.n_outputs_)
+                ]
+
+        elif self.strategy == "quantile":
+            if self.quantile is None:
+                raise ValueError(
+                    "When using `strategy='quantile', you have to specify the desired "
+                    "quantile in the range [0, 1]."
+                )
+            percentile = self.quantile * 100.0
+            if sample_weight is None:
+                self.constant_ = np.percentile(y, axis=0, q=percentile)
+            else:
+                self.constant_ = [
+                    _weighted_percentile(y[:, k], sample_weight, percentile=percentile)
+                    for k in range(self.n_outputs_)
+                ]
+
+        elif self.strategy == "constant":
+            if self.constant is None:
+                raise TypeError(
+                    "Constant target value has to be specified "
+                    "when the constant strategy is used."
+                )
+
+            self.constant_ = check_array(
+                self.constant,
+                accept_sparse=["csr", "csc", "coo"],
+                ensure_2d=False,
+                ensure_min_samples=0,
+            )
+
+            if self.n_outputs_ != 1 and self.constant_.shape[0] != y.shape[1]:
+                raise ValueError(
+                    "Constant target value should have shape (%d, 1)." % y.shape[1]
+                )
+
+        self.constant_ = np.reshape(self.constant_, (1, -1))
         return self
